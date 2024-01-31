1860945a74c9ae3ba23a039799f5b9de584775c3
JoseVides17 <108839780+JoseVides17@users.noreply.github.com>
2024-01-31 15:10:18 -0500

    hola mundo

--- src/main/java/jose/prueba/PruebaApplication.java
+++ src/main/java/jose/prueba/PruebaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PruebaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hola Mundo!");
 		SpringApplication.run(PruebaApplication.class, args);
 	}
 
